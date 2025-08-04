5cfc04b8bdf8c150457c55690fe952d099fcc74a
Hri <hri.gh@outlook.com>
2025-08-04 15:08:38 +0530

    feat: switch authentication adapter from MongoDB to Prisma and update dependencies

--- auth.ts
+++ auth.ts
@@ -1,9 +1,11 @@
 import NextAuth from "next-auth"
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import {PrismaAdapter} from "@auth/prisma-adapter"
+import { Prisma } from "@prisma/client"
 import client from "./lib/mongodb"
 import authConfig from "./auth.config"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: MongoDBAdapter(client),
+    adapter: PrismaAdapter(Prisma),
     ...authConfig
 })
