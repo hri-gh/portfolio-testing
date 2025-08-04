603214a506cb26f82da54357b66df6f8e8dadf02
Hri <hri.gh@outlook.com>
2025-08-04 15:10:47 +0530

    refactor: update database connection configuration for Prisma with new environment variables

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,25 +1,34 @@
-// lib/prismadb.ts
+// // lib/prismadb.ts
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaLibSQL } from "@prisma/adapter-libsql";
+
+// const adapter = new PrismaLibSQL({
+//   url: process.env.DATABASE_URL!,
+//   authToken: process.env.TURSO_AUTH_TOKEN!,
+// });
+
+// declare global {
+//   var prismadb: PrismaClient | undefined;
+// }
+
+// let prismadb: PrismaClient;
+
+// if (process.env.NODE_ENV === "production") {
+//   prismadb = new PrismaClient({ adapter } as any);
+// } else {
+//   if (!globalThis.prismadb) {
+//     globalThis.prismadb = new PrismaClient({ adapter } as any);
+//   }
+//   prismadb = globalThis.prismadb;
+// }
+
+// export default prismadb;
+
 import { PrismaClient } from "@prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
 
 const adapter = new PrismaLibSQL({
-  url: process.env.DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!,
-});
-
-declare global {
-  var prismadb: PrismaClient | undefined;
-}
-
-let prismadb: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prismadb = new PrismaClient({ adapter } as any);
-} else {
-  if (!globalThis.prismadb) {
-    globalThis.prismadb = new PrismaClient({ adapter } as any);
-  }
-  prismadb = globalThis.prismadb;
-}
-
-export default prismadb;
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+})
+const prismadb = new PrismaClient({ adapter })
